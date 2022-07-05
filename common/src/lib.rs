97fb480e77868eda5513292bf827884bcb86e92a
Forest Anderson <forestkzanderson@gmail.com>
2022-07-05 19:52:33 -0400

    Initial crates

---
+++ common/src/lib.rs
@@ -0,0 +1,15 @@
+pub struct Message {
+    pub id: u32,
+    pub name: String,
+    pub content: String,
+    pub timestamp: u64,
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
