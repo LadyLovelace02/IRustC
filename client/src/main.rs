060af1b788601ec6aceb3f98553f8761bca943f0
Melissa <melissa@rand-family.com>
2022-08-06 19:02:37 -0400

    can connect to a server

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,21 +1,21 @@
+use common::Message;
+use serde::{Deserialize, Serialize};
 use std::io::prelude::*;
 use std::net::TcpStream;
-use serde::{Serialize, Deserialize};
-use common::Message;
 fn connect_to_server(ip: String, port: String) {
     let connection_ip_and_port: String = format!("{}:{}", ip, port);
     if let Ok(stream) = TcpStream::connect(connection_ip_and_port) {
-        println!("CONNECTED");//testing connection
-    }
-    else {
+        println!("CONNECTED"); //testing connection
+    } else {
         println!("CONNECTION FAILED");
     }
 }
 
-fn send_a_message(stream: TcpStream, message: Message) {
-    stream.write(serde_json::to_string(&message).unwrap());//send a message to the server
+fn send_a_message(mut stream: TcpStream, message: Message) {
+    stream.write(&serde_json::to_string(&message).unwrap().as_bytes()); //send a message to the server
 }
 
 fn main() {
+    connect_to_server("127.0.0.1".to_string(), "2234".to_string());
     println!("Hello, world!");
 }
--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -7,8 +7,6 @@ pub struct Migrator;
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![
-            Box::new(m20220716_182324_initial_tables::Migration),
-        ]
+        vec![Box::new(m20220716_182324_initial_tables::Migration)]
     }
 }
