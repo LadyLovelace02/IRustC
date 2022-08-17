8fa22230f265b269859448e871278318bd7b25e1
Melissa <melissa@rand-family.com>
2022-08-17 16:30:59 -0400

    refactored and cleaned

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,16 +1,6 @@
-/// A simple example demonstrating how to handle user input. This is
-/// a bit out of the scope of the library as it does not provide any
-/// input handling out of the box. However, it may helps some to get
-/// started.
-///
-/// This is a very simple example:
-///   * A input box always focused. Every character you type is registered
-///   here
-///   * Pressing Backspace erases a character
-///   * Pressing Enter pushes the current input in the history of previous
-///   messages
+use client::Client;
 use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    event::{DisableMouseCapture, EnableMouseCapture},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
@@ -20,49 +10,21 @@ use std::{
     net::TcpStream,
 };
 use tui::{
-    backend::{Backend, CrosstermBackend},
-    layout::{Constraint, Direction, Layout},
-    style::{Color, Modifier, Style},
-    text::{Span, Spans, Text},
-    widgets::{Block, Borders, List, ListItem, Paragraph},
-    Frame, Terminal,
+    backend::{Backend, CrosstermBackend}, Terminal,
 };
-use unicode_width::UnicodeWidthStr;
+
 
 use common::Message;
 
 use std::io::prelude::*;
 
-struct Client {
-    stream: TcpStream,
-}
-impl Client {
-    /// initialize a connection to the server at `ip` and `port`
-    /// returns an error if the connection failed
-    fn connect_to_server(ip: String, port: String) -> Result<Self, ConnectionError> {
-        let connection_ip_and_port: String = format!("{}:{}", ip, port);
-        if let Ok(stream) = TcpStream::connect(connection_ip_and_port) {
-            println!("CONNECTED"); //testing connection
-                                   // return a client with the new connection
-            return Ok(Client { stream });
-        } else {
-            println!("CONNECTION FAILED");
-            // return an error saying we failed
-            return Err(ConnectionError::Failed);
-        }
-    }
+pub mod app;
+pub mod ui;
+pub mod client;
 
-    // after looking, this can probably be removed, unless theres a reason to be sending a message with this client, but to a different tcp stream
-    fn send_a_message(&self, stream: &mut TcpStream, message: Message) {
-        stream.write(&serde_json::to_string(&message).unwrap().as_bytes()); //send a message to the server
-    }
+use app::run_app;
 
-    /// use this to send messages to this client
-    fn send_a_message_to_self(&mut self, message: Message) {
-        self.stream
-            .write(&serde_json::to_string(&message).unwrap().as_bytes()); //send a message to the server
-    }
-}
+
 
 /// App holds the state of the application
 struct App {
@@ -140,90 +102,9 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 #[derive(Debug)]
-enum ConnectionError {
+pub enum ConnectionError {
     Failed,
 }
 
-fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
-    loop {
-        terminal.draw(|f| ui(f, &app))?;
 
-        if let Event::Key(key) = event::read()? {
-            match key.code {
-                // When the enter key is pressed, send the message to the server
-                KeyCode::Enter => {
-                    app.messages.push(app.input.drain(..).collect());
-                }
-                // If any character key is pressed, add it to a buffer
-                KeyCode::Char(c) => {
-                    app.input.push(c);
-                }
-                KeyCode::Backspace => {
-                    app.input.pop();
-                }
-                // Escape exits the application
-                KeyCode::Esc => {
-                    return Ok(());
-                }
-                _ => {}
-            }
-        }
-    }
-}
 
-fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
-    // There will be 2 chu
-    let chunks = Layout::default()
-        .direction(Direction::Vertical)
-        .margin(2)
-        .constraints(
-            [
-                Constraint::Length(1),
-                Constraint::Min(1),
-                Constraint::Length(3),
-            ]
-            .as_ref(),
-        )
-        .split(f.size());
-
-    let mut text = Text::from(Spans::from(vec![
-        Span::raw("Press "),
-        Span::styled("Esc", Style::default().add_modifier(Modifier::BOLD)),
-        Span::raw(" to exit"),
-    ]));
-
-    text.patch_style(Style::default());
-    let help_message = Paragraph::new(text);
-    f.render_widget(help_message, chunks[0]);
-
-    // Show the cursor
-    f.set_cursor(
-        // Put cursor past the end of the input text
-        chunks[2].x + app.input.width() as u16 + 1,
-        // Move one line down, from the border to the input line
-        chunks[2].y + 1,
-    );
-
-    // Render the messages
-    let messages: Vec<ListItem> = app
-        .messages
-        .iter()
-        .enumerate()
-        .map(|(i, m)| {
-            let content = vec![Spans::from(Span::raw(format!("{}: {}", i, m)))];
-            ListItem::new(content)
-        })
-        .collect();
-
-    let messages =
-        List::new(messages).block(Block::default().borders(Borders::ALL).title("Messages"));
-
-    f.render_widget(messages, chunks[1]);
-
-    // Render the input box
-    let input = Paragraph::new(app.input.as_ref())
-        .style(Style::default().fg(Color::Yellow))
-        .block(Block::default().borders(Borders::ALL).title("Input"));
-
-    f.render_widget(input, chunks[2]);
-}
